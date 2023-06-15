505e837850537c5a7862665d3044b8637f7a9199
mishi93999 <134986097+mishi93999@users.noreply.github.com>
2023-06-15 12:35:53 +0900

    Add files via upload
    update data location

--- seoul_environment.py
+++ seoul_environment.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Seoul Air Pollution Dashboard", page_icon=":bar_chart:", layout="wide")
 
-df = pd.read_csv('./data/seoul_dust_added.csv', encoding = 'cp949')
+df = pd.read_csv('./seoul_data/seoul_dust_added.csv', encoding = 'cp949')
 columns = df.columns
 
 #---SIDE Bar---
